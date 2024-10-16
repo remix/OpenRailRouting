d71727c128c97a8897b03926f313e7db9ca84474
Sachin Goel <sachin.goel@ridewithvia.com>
2024-10-16 12:05:48 -0700

    Remove `web` from build script
    We don't need the `web` project for our needs at Remix

--- build.sh
+++ build.sh
@@ -20,10 +20,9 @@ cd graphhopper
 echo "Cleaning"
 mvn clean
 echo "Installing"
-mvn --projects web,core,web-bundle,map-matching,web-api -P include-client-hc -am -DskipTests=true compile package install
+mvn --projects core,web-bundle,map-matching,web-api -P include-client-hc -am -DskipTests=true compile package install
 echo "Deploying"
 mvn deploy:deploy-file -Durl=file://"$MAVEN_REPO" -Dfile=core/target/graphhopper-core-$GH_VERSION.jar -DgroupId=com.graphhopper -DartifactId=graphhopper-core -Dpackaging=jar -Dversion=$GH_VERSION
-mvn deploy:deploy-file -Durl=file://"$MAVEN_REPO" -Dfile=web/target/graphhopper-web-$GH_VERSION.jar -DgroupId=com.graphhopper -DartifactId=graphhopper-web -Dpackaging=jar -Dversion=$GH_VERSION
 mvn deploy:deploy-file -Durl=file://"$MAVEN_REPO" -Dfile=web-bundle/target/graphhopper-web-bundle-$GH_VERSION.jar -DgroupId=com.graphhopper -DartifactId=graphhopper-web-bundle -Dpackaging=jar -Dversion=$GH_VERSION
 mvn deploy:deploy-file -Durl=file://"$MAVEN_REPO" -Dfile=web-api/target/graphhopper-web-api-$GH_VERSION.jar -DgroupId=com.graphhopper -DartifactId=graphhopper-web-api -Dpackaging=jar -Dversion=$GH_VERSION
 mvn deploy:deploy-file -Durl=file://"$MAVEN_REPO" -Dfile=map-matching/target/graphhopper-map-matching-$GH_VERSION.jar -DgroupId=com.graphhopper -DartifactId=graphhopper-map-matching -Dpackaging=jar -Dversion=$GH_VERSION
